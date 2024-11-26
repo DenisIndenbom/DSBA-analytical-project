843e722b9a6cc572e9225e6238f4535338e4cc68
Denis Indenbom <59681620+denisindenbom@users.noreply.github.com>
2024-11-27 00:18:48 +0300

    Have finished moving notebook content to streamlit web app

--- webapp/main.py
+++ webapp/main.py
@@ -18,6 +18,8 @@ def load_data():
     df['month'] = df['date'].dt.month
     df['day'] = df['date'].dt.day
 
+    df['destructive'] = df['magnitudo'].apply(lambda x:np.log10(max(1, x))) * df['significance']
+
     return df
 
 
@@ -33,6 +35,16 @@ def hist(df, bins=None, title='', x_label=None, y_label=None, log=False):
     plt.ylabel(y_label)
 
     ax.hist(df, bins=bins, log=log)
+
+    st.pyplot(fig)
+
+
+def pie(df, title=''):
+    fig, ax = plt.subplots()
+
+    plt.title(title)
+
+    ax.pie(df, labels=df.index.tolist())
 
     st.pyplot(fig)
 
@@ -92,29 +104,75 @@ hist(data['depth'], bins=np.arange(data['depth'].min(), data['depth'].max(), 50)
 st.subheader('The trend of the quantile (80%) over time')
 
 center_title_h5('Quantile trend of magnitude')
-st.line_chart(data.groupby('year').agg({'magnitudo': lambda x: x.quantile(0.8)}), x_label='Year', y_label='Magnitudo')
+st.line_chart(data.groupby('year').agg({'magnitudo':lambda x:x.quantile(0.8)}), x_label='Year', y_label='Magnitudo')
 
 center_title_h5('Quantile trend of significance')
-st.line_chart(data.groupby('year').agg({'significance': lambda x: x.quantile(0.8)}), x_label='Year',
+st.line_chart(data.groupby('year').agg({'significance':lambda x:x.quantile(0.8)}), x_label='Year',
               y_label='Significance')
 
 center_title_h5('Quantile trend of depth')
-st.line_chart(data.groupby('year').agg({'depth': lambda x: x.quantile(0.8)}), x_label='Year', y_label='Depth')
+st.line_chart(data.groupby('year').agg({'depth':lambda x:x.quantile(0.8)}), x_label='Year', y_label='Depth')
 
 st.subheader('The trend of the maximal value over time')
 
 center_title_h5('Max trend of magnitude')
-st.line_chart(data.groupby('year').agg({'magnitudo': 'max'}, x_label='Year', y_label='Magnitudo'))
+st.line_chart(data.groupby('year').agg({'magnitudo':'max'}, x_label='Year', y_label='Magnitudo'))
 
 center_title_h5('Max trend of significance')
-st.line_chart(data.groupby('year').agg({'significance': 'max'}), x_label='Year',
+st.line_chart(data.groupby('year').agg({'significance':'max'}), x_label='Year',
               y_label='Significance')
 
 center_title_h5('Max trend of depth')
-st.line_chart(data.groupby('year').agg({'depth': 'max'}), x_label='Year', y_label='Depth')
+st.line_chart(data.groupby('year').agg({'depth':'max'}), x_label='Year', y_label='Depth')
 
 st.header('Hypothesis')
 st.markdown('**Tsunamis are more destructive than earthquakes.**')
 st.markdown("""**Destructive definition**
 
 $destructive = \\log(\\max(1, magnitudo))*significance$""")
+
+st.subheader('Analysis')
+
+center_title_h5('Mean of destructive')
+st.bar_chart(data.groupby('tsunami').agg({'destructive':'mean'}), x_label='Tsunami', y_label='Destructive')
+st.markdown('*As we can see, on average tsunami is more destructive than ordinary earthquakes.*')
+
+center_title_h5('Quantile (50%) of destructive')
+st.bar_chart(data.groupby('tsunami').agg({'destructive':lambda x:x.quantile(0.5)}), x_label='Tsunami',
+             y_label='Destructive')
+st.markdown(
+    '*Here we see, 50 percent of tsunamis are more destructive than earthquakes. Also, we see that destructive of 50 percent of earthquakes less than 50 points.*')
+
+center_title_h5('Quantile (90%) of destructive')
+st.bar_chart(data.groupby('tsunami').agg({'destructive':lambda x:x.quantile(0.9)}), x_label='Tsunami',
+             y_label='Destructive')
+st.markdown('*In this case, the 10 percent of tsunamis are more destructive than earthquakes*')
+
+center_title_h5('Maximal value of destructive')
+st.bar_chart(data.groupby('tsunami').agg({'destructive':'max'}), x_label='Tsunami', y_label='Destructive')
+st.markdown(
+    '*The most destructive of the recorded tsunamis was more powerful than the most destructive of the recorded earthquakes.*')
+
+st.subheader('Some statistics for conclusion')
+
+tsunami = data[data['tsunami'] == 1]
+
+pie(tsunami['state'].value_counts().head(15), title='The number of tsunamis in different states')
+pie(tsunami[tsunami['destructive'] >= tsunami['destructive'].mean()]['state'].value_counts().head(15),
+    title='The number of significance tsunamis in different states')
+pie(tsunami[tsunami['destructive'] >= tsunami['destructive'].max() // 2]['state'].value_counts().head(15),
+    title='The number of destructive tsunamis in different states')
+
+st.markdown("""
+## Hypothesis has been proved
+
+**Why?**
+
+- The most destructive earthquake was a tsunami. $\triangle max=63.2$ (**Maximal value of destructive** plot)
+- The average tsunami is more destructive than the average earthquake. (**Mean of destructive** plot)
+- The 90 percent of tsunamis are more destructive than the 10 percent of earthquakes. (**Quantile (90%) of destructive** plot)
+
+### Conclusion
+
+As we can see, tsunamis often occur in Alaska, but they are usually not destructive. Destructive events typically happen in Japan, Mexico and Chile.
+""")
