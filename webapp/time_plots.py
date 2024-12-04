fa4983ed21d6f1f6aae458bebe3c7d518c8bfd87
denis <denis.indenbom@yandex.ru>
2024-12-04 12:33:56 +0300

    Implemented interactive page which draws user time plots

--- webapp/time_plots.py
+++ webapp/time_plots.py
@@ -10,47 +10,50 @@ st.title('Interactive Graphics')
 st.sidebar.header('Select Chart Options')
 chart_type = st.sidebar.selectbox(
     'Choose chart type',
-    ['Line Chart', 'Bar Chart', 'Scatter Plot']
+    ['Not selected', 'Line Chart', 'Bar Chart']
 )
 
 data = load_data()
-
-# Common customization options
-st.sidebar.subheader('Common Options')
-y_axis = st.sidebar.selectbox('Choose Y-axis', ['magnitudo'])
 
 chart = None
 
 # Conditional input fields for chart-specific options
 if chart_type == 'Line Chart':
-    st.sidebar.subheader("Line Chart Options")
+    st.sidebar.subheader('Common Options')
+    y_axis = st.sidebar.selectbox('Choose Y-axis', ['magnitudo', 'significance', 'depth', 'destructive'])
+
+    st.sidebar.subheader('Line Chart Options')
     agg_function = st.sidebar.selectbox('Aggregate functions', ['mean', 'median', 'max'])
-    df = data.groupby('year').agg({y_axis: agg_function})
+    df = data.groupby('year').agg({y_axis: agg_function}).reset_index()
 
-    chart = alt.Chart(df).mark_line().encode(x='year', y=y_axis)
+    chart = alt.Chart(df).mark_line().encode(
+        x=alt.X('year', title='Year'),
+        y=alt.Y(y_axis, title=y_axis)
+    ).properties(title=f'Trend of {agg_function} {y_axis}')
 
-# elif chart_type == 'Bar Chart':
-#     st.sidebar.subheader('Bar Chart Options')
-#     bar_width = st.sidebar.slider('Bar Width', 0.2, 1.0, 0.5)
-#
-#     chart = alt.Chart(data).mark_bar(size=bar_width * 50).encode(
-#         x=alt.X('x:O', title=x_axis_label),
-#         y=alt.Y('y', title=y_axis_label),
-#         color='category'
-#     ).properties(title=title)
-#
-# elif chart_type == 'Scatter Plot':
-#     st.sidebar.subheader('Scatter Plot Options')
-#     size_by_value = st.sidebar.checkbox('Size by Value', value=False)
-#
-#     chart = alt.Chart(data).mark_circle().encode(
-#         x=alt.X('x', title=x_axis_label),
-#         y=alt.Y('y', title=y_axis_label),
-#         size='y' if size_by_value else alt.value(60),
-#         color='category'
-#     ).properties(title=title)
+elif chart_type == 'Bar Chart':
+    st.sidebar.subheader('Bar Chart Options')
+
+    feature = st.sidebar.selectbox('Feature', ['No feature', 'magnitudo', 'significance', 'depth', 'destructive'])
+
+    if feature != 'No feature':
+        threshold = st.sidebar.slider('Threshold',
+                                      value=float(data[feature].mean()),
+                                      min_value=float(data[feature].min()),
+                                      max_value=float(data[feature].max()),
+                                      step=0.1)
+
+        df = data['year'][data[feature] >= threshold].value_counts(sort=False).reset_index()
+    else:
+        df = data['year'].value_counts(sort=False).reset_index()
+
+    chart = alt.Chart(df).mark_bar(size=15).encode(
+        x=alt.X('year', title='Year'),
+        y=alt.Y('count', title='Count')
+    ).properties(title=f'Number of earthquake events' + (f' filtered by {feature}' if feature != 'No feature' else ''))
 
 # Display chart
-st.altair_chart(chart, use_container_width=True)
-
-st.write('Done!')
+if chart is not None:
+    st.altair_chart(chart, use_container_width=True)
+else:
+    st.markdown('*Choose a chart!*')
